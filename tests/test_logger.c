d1fb2c15980c555075c91110673c0c6ebc43b788
Trung Pham <pbtrung@outlook.com>
2025-09-03 20:30:36 +0700

    Use proper error codes

--- tests/test_logger.c
+++ tests/test_logger.c
@@ -2,6 +2,7 @@
 #include <apr_pools.h>
 
 #include "apr_logger.h"
+#include "err.h"
 
 int main() {
     apr_initialize();
@@ -10,11 +11,12 @@ int main() {
     apr_pool_create(&pool, NULL);
 
     apr_logger_t *logger;
-    if (apr_logger_init(&logger, pool, "test.log") != APR_SUCCESS)
-        return 1;
+    data_status_t rv = apr_logger_init(&logger, pool, "test.log");
+    if (rv != DATA_SUCCESS)
+        return rv;
 
     apr_logger_shutdown(logger);
     apr_pool_destroy(pool);
     apr_terminate();
-    return 0;
+    return DATA_SUCCESS;
 }
